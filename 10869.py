8fefb7767e99a55005ebc9291f9ab9daf7cef5ab
yubin08 <88960607+yubin08@users.noreply.github.com>
2022-01-23 14:25:06 +0900

    Add files via upload

---
+++ 10869.py
@@ -0,0 +1,8 @@
+# A, B = input().split()
+A = 7
+B = 3
+print(int(A)+int(B))
+print(int(A)-int(B))
+print(int(A)*int(B))
+print(int(int(A)/int(B)))
+print(int(A)%int(B))
---
+++ 2588.py
@@ -0,0 +1,8 @@
+A = int(input())
+B1 = int(input())
+B = list(str(B1))
+
+print(A*int(B[2]))
+print(A*int(B[1]))
+print(A*int(B[0]))
+print(A*B1)
